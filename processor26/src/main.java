cc90166534144c3cda043fdf35a8a41b32a1b3df
2e12 <gabriel.beutler@4teamwork.ch>
2020-03-26 14:06:54 +0100

    Read csv file line by line.

--- processor26/src/main.java
+++ processor26/src/main.java
@@ -1,16 +1,47 @@
-import java.io.File;
+import java.io.*;
 
 public class main {
-    public static void main(String[] args) {
-        if(args.length >= 1){
-            File file = new File(args[0]);
-            if(file.exists()){
+    String filePath;
 
-            }else{
-                System.out.println("Error: File not found.");
+    /**
+     * This functions checks if a valid file path is provided.
+     * @param args
+     */
+    public static void main(String[] args) {
+        if(args.length < 1){
+            System.out.println("Error: No file provided.");
+            return;
+        }
+
+        File file = new File(args[0]);
+        if (file.exists()) {
+            new main(file.getAbsolutePath());
+        } else {
+            System.out.println("Error: File not found.");
+        }
+    }
+
+    /**
+     * Set everything up.
+     * @param filePath
+     */
+    public main(String filePath) {
+        this.filePath = filePath;
+        this.convertCsvToList();
+    }
+
+    /**
+     * Converts the csv file provided in this.filePath to a Person ArrayList.
+     */
+    public void convertCsvToList(){
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(this.filePath));
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-        }else{
-            System.out.println("Error: Please specify a file path.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
