160dff127822e8e5d7455588aa3427677d8fe594
Ha21iko <haikonudingmorales@yahoo.com>
2020-03-26 16:47:37 +0100

    Filter Functions

--- processor26/src/Person.java
+++ processor26/src/Person.java
@@ -1,4 +1,4 @@
-import java.util.Date;
+import java.util.*;
 
 enum Gender{
     FEMALE, MALE;
@@ -30,6 +30,111 @@ public class Person implements Comparable{
 
     public double getBMI(){
         return this.weight / (this.height * this.height);
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * Function to get all readed persons.
+     * @param  persons list of persons
+     * @return numbers of all persons
+     */
+    public int getAmountPersons(ArrayList<Person> persons){
+        return persons.size();
+    }
+
+    /**
+     * Function returns the number of females in a person list.
+     * @param persons list of persons
+     * @return number of all females
+     */
+    public int getAmountFemalePersons(ArrayList<Person> persons){
+        int femaleCounter = 0;
+        for (int  i = 0; i < persons.size(); i++){
+            if (persons.get(i).getGender() == Gender.FEMALE){
+                femaleCounter ++;
+            }
+        }
+        return femaleCounter;
+    }
+
+    /**
+     * Function returns the number of male in a person list.
+     * @param persons list of persons.
+     * @return number of all males
+     */
+    public int getAmountMalePersons(ArrayList<Person> persons){
+        int maleCounter = 0;
+        for (int i = 0; i < persons.size(); i++){
+            if (persons.get(i).getGender() == Gender.MALE){
+                maleCounter ++;
+            }
+        }
+        return maleCounter;
+    }
+
+
+    public Person getYoungestPerson(ArrayList<Person> persons){
+        Date youngest = persons.get(0).getBirthday();
+
+        Calendar calendar = new GregorianCalendar();
+        Calendar compare = new GregorianCalendar();
+        int indexYoungest = 0;
+        for (int i = 0; i < persons.size(); i++){
+            calendar.setTime(persons.get(i).getBirthday());
+            compare.setTime(youngest);
+            if (calendar.get(Calendar.YEAR) > compare.get(Calendar.YEAR)){
+                youngest = persons.get(i).getBirthday();
+                indexYoungest = i;
+            }
+            else if(calendar.get(Calendar.YEAR) == compare.get(Calendar.YEAR)){
+                if (calendar.get(Calendar.MONTH) > compare.get(Calendar.MONTH)){
+                    youngest = persons.get(i).getBirthday();
+                    indexYoungest = i;
+                }
+                if (calendar.get(Calendar.MONTH) == compare.get(Calendar.MONTH)){
+                    if (calendar.get(Calendar.DAY_OF_MONTH) >= compare.get(Calendar.DAY_OF_MONTH)){
+                        youngest = persons.get(i).getBirthday();
+                        indexYoungest = i;
+                    }
+                }
+            }
+        }
+        return persons.get(indexYoungest);
+    }
+
+    public Person getOldestPerson(ArrayList<Person> persons){
+        Date oldest = new Date();
+        Calendar calendar = new GregorianCalendar();
+        Calendar compare = new GregorianCalendar();
+        int indexOldest = 0;
+        for (int i = 0; i < persons.size(); i++){
+            calendar.setTime(persons.get(i).getBirthday());
+            compare.setTime(oldest);
+            if (calendar.get(Calendar.YEAR)< compare.get(Calendar.YEAR)){
+                oldest = persons.get(i).getBirthday();
+                indexOldest = i;
+            }
+            else if(calendar.get(Calendar.YEAR) == compare.get(Calendar.YEAR)){
+                if (calendar.get(Calendar.MONTH) < compare.get(Calendar.MONTH)){
+                    oldest = persons.get(i).getBirthday();
+                    indexOldest = i;
+                }
+                if (calendar.get(Calendar.MONTH) == compare.get(Calendar.MONTH)){
+                    if (calendar.get(Calendar.DAY_OF_MONTH) <= compare.get(Calendar.DAY_OF_MONTH)){
+                        oldest = persons.get(i).getBirthday();
+                        indexOldest = i;
+                    }
+                }
+            }
+        }
+        return persons.get(indexOldest);
     }
 
     @Override
