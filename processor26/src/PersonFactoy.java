acf318744e698249b371e39d4fb3865528b8bd6f
2e12 <gabriel.beutler@4teamwork.ch>
2020-03-26 15:04:25 +0100

    Add person factory.

--- processor26/src/PersonFactoy.java
+++ processor26/src/PersonFactoy.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PersonFactoy {
+    public static Person getPersonFromCsvLine(String line) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yy");
+
+        String[] col = line.split(",");
+        int id = Integer.valueOf(col[0]);
+        String email = col[1];
+        String lastName = col[2];
+        String firstName = col[3];
+        Gender gender = null;
+        if(col[4] == "Male"){
+            gender = Gender.MALE;
+        }
+        if(col[4] == "Female"){
+            gender = Gender.FEMALE;
+        }
+        int height = Integer.valueOf(col[5]);
+        int weight = Integer.valueOf(col[6]);
+
+        Date birthday = new Date();
+        try {
+            birthday = formatter.parse(col[7]);
+        } catch (ParseException e) {
+            System.out.println("Error: Malformed date: " + line);
+        }
+        Person person = new Person(
+                id,
+                email,
+                lastName,
+                firstName,
+                gender,
+                height,
+                weight,
+                birthday
+        );
+        return person;
+    }
 }
